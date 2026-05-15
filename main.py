6a5e1186d5222e87c029f1ec5ce5151770353f53
Sohaib Waheed <expk0322@pakoxygen.com>
2026-05-15 10:45:31 +0500

    updated version

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from router import router
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Carton-Counter", version="1.2.0")
+app = FastAPI(title="Carton-Counter", version="1.2.1")
 
 # CORS
 app.add_middleware(
